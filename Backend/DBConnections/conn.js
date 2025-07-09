facc26070a9002fe91d3d1d65311fa64cccacbbd
Sameer69113 <156511626+Sameer69113@users.noreply.github.com>
2025-07-09 20:06:02 +0530

    Database created sucessfully

--- Backend/DBConnections/conn.js
+++ Backend/DBConnections/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/TigerGymBackend")
+  .connect("mongodb+srv://Sameersameer:Sameersameer@cluster0.uwf2d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("Connected to MongoDB");
   })
