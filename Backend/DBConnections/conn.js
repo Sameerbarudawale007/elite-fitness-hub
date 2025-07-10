948c0c0285cdf3e68be5ccfa2680bdbcb70254d8
Sameer69113 <156511626+Sameer69113@users.noreply.github.com>
2025-07-10 10:24:07 +0530

    Connected to Atlas

--- Backend/DBConnections/conn.js
+++ Backend/DBConnections/conn.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/TigerGymBackend")
+  .connect(
+    "mongodb+srv://Sameersameer:Sameersameer@cluster0.uwf2d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Connected to MongoDB");
   })
